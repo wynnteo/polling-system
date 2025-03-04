2ab66e4ad64e9ce3944f948ccfd3b78da083afa7
Wynn Teo <wynnteo@Wynns-MacBook-Pro.local>
2025-03-04 10:29:16 +0800

    revert

--- backend/src/main/java/com/nnywtech/polling/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.nnywtech.polling.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
